ee596ac207048ca6e2143c995c5e271d08cac8b4
Saurya Velagapudi <saurya@gmail.com>
2024-11-26 18:27:12 -0800

    Create script that generates a new post for today

---
+++ new_post.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "---
+layout: post
+title: Restarting this blog?
+date: 2024-11-26 10:00:21 -0800
+description: 
+img: # Add image post (optional)
+fig-caption: # Add figcaption (optional)
+tags: [Writing]
+---" > ./_posts/$(date -Idate)-new-post.markdown
+

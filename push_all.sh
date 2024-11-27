14821b6a1f0f8298c4813f4df19aa12bc4c73566
Saurya Velagapudi <saurya@gmail.com>
2024-11-26 21:47:49 -0800

    Add more detail to push_all script

--- push_all.sh
+++ push_all.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# This only works if you've run
+# git remote set-url github git@github.com:saurya/flexible-jekyll.git
 git push github
+# This assumes that origin is set to
+# git remote origin saurya_saur@ssh.nyc1.nearlyfreespeech.net:home/private/git/BLOG.git
+# Probably best to just pull this repo from NFSN rather than Github
 git push origin

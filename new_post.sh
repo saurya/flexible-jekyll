1a719b85782c2dedcb392139592dc49b0b9bbade
Saurya Velagapudi <saurya@gmail.com>
2024-12-06 22:29:20 -0800

    Fixed up new post date situation

--- new_post.sh
+++ new_post.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 TODAYS_DATE=$(date "+%Y-%m-%d %H:%M:%S %z")
 
-FILE_NAME=$TODAYS_DATE"-"$(echo $@ | sed -e 's/ /-/g')".markdown"
+FILE_NAME=$(date "+%Y-%m-%d")"-"$(echo $@ | sed -e 's/ /-/g')".markdown"
 echo $FILE_NAME
 echo "---
 layout: post
 title: $@
-date: $TODAYS_DATE 10:00:21 -0800
+date: $TODAYS_DATE
 description: 
 img: # Add image post (optional)
 fig-caption: # Add figcaption (optional)
